9a3743bba9e0369b0f276eadbf11f57a0507d789
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-17 23:23:05 -0800

    user can join and disconnect via socket

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import React from 'react';
-// import Swiper from './pages/Swiper';
-// import Deck from './components/Deck';
-// import { BrowserRouter, Route } from 'react-router-dom'; // TODO
-import Messages from './components/Messages';
-import './App.css';
+import React from "react";
+import Messages from "./components/Messages";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
 function App() {
-  /**
-   * Right now this is returning a responsive layout
-   * this is Chris, I think this needs to be improved upon, but hopefully
-   * it's a decent start. 
-   * TODO:
-   * 1. Create a layout component to move this code to
-   * 2. Add <BrowserRouter></BrowserRouter> stuff to this component
-   * 3. Ask for help early and often :)
-   */
   return (
-    <div className="container-fluid min-w-full flex min-h-screen bg-red-200" > {/* Container */}
-      {/** App should probably be the Router stuff, this is layout stuff i know, TODO **/}
-      < div className="bg-green-400 w-full sm:w-1/4" > {/** Left Column */}
-        {/** TODO
-         * if location.pathname /app/messages
-         * return <Messages />
-         */}
+    <div className="container-fluid min-w-full flex min-h-screen bg-red-200">
+      <div className="bg-green-400 w-full sm:w-1/4">
         <Messages />
       </div>
-      <div className="bg-blue-400 w-3/4 lg:w-1/2 hidden sm:block">{/** Middle / Main */}
+      <div className="bg-blue-400 w-3/4 lg:w-1/2 hidden sm:block">
         <h3>Middle Column</h3>
       </div>
-      <div className="bg-yellow-400 md:w-1/4 hidden lg:block">{/** Right Column */}
+      <div className="bg-yellow-400 md:w-1/4 hidden lg:block">
         <h3>Right Column</h3>
+        <Router>
+          <Route path="/api/api/chat/join" component={Join} />
+          <Route path="/api/api/chat/chat" component={Chat} />
+        </Router>
       </div>
     </div>
   );

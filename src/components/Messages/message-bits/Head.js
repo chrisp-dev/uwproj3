0fa0d11e31a9230a9ea97245f7f6e4517dda2fd2
jittel <nathan.c.ginn@gmail.com>
2020-02-18 12:39:02 -0800

    idk

--- src/components/Messages/message-bits/Head.js
+++ src/components/Messages/message-bits/Head.js
@@ -7,6 +7,8 @@ export default function Head (props) {
         <div className='row flex horizontal'>
             {/* TODO: make it so that it loads however many matches you have */}
             <Image image={props.message.image}/>
+            <Image image={props.message.image}/>
+            <Image image={props.message.image}/>
             {/* <Image message={props.message}/> */}
             {/* <Image message={props.message}/> */}
         </div>

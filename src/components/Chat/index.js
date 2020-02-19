1c7cb63d958758a5dcc548adcbc01cbbcb83c7b8
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-19 10:27:08 -0800

    disable heroku for now

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -17,7 +17,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "https://project-chat-application.herokuapp.com/";
+  // const ENDPOINT = "https://project-chat-application.herokuapp.com/";
+  const ENDPOINT = "localhost:8080";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);

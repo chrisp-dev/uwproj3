bf65a33a301d6b489dc60059c8aa5e1b55bd8524
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-18 19:44:25 -0800

    chat app client side

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,36 +1,73 @@
 import React, { useState, useEffect } from "react";
-import "./style.css";
-import io from "socket.io-client";
 import queryString from "query-string";
+import io from "socket.io-client";
+
+import TextContainer from "../TextContainer";
+import Messages from "../Messages";
+import InfoBar from "../InfoBar";
+import Input from "../Input";
+
+import "./style.css";
 
 let socket;
 
-export default function Chat({ location }) {
-  const [senderName, setSenderName] = useState("");
-  const [receiverName, setReceiverName] = useState("");
-  const ENDPOINT = "localhost:8080";
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "https://project-chat-application.herokuapp.com/";
 
   useEffect(() => {
-    const { senderName, receiverName } = queryString.parse(location.search);
+    const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
-    console.log(socket);
+    setRoom(room);
+    setName(name);
 
-    setSenderName(senderName);
-    setReceiverName(receiverName);
+    socket.emit("join", { name, room }, error => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
 
-    socket.emit("join", { senderName, receiverName }, () => {});
+  useEffect(() => {
+    socket.on("message", message => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
 
     return () => {
       socket.emit("disconnect");
+
       socket.off();
     };
-  }, [ENDPOINT, location.search]);
+  }, [messages]);
+
+  const sendMessage = event => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
 
   return (
-    <div className="Chat">
-      <h1>Chat Component</h1>
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users} />
     </div>
   );
-}
+};
+
+export default Chat;

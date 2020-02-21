626f1af950c7fb00812b068213ae05cc63a0efb5
Chris Paradeise <chris.paradeise@gmail.com>
2020-02-21 14:23:13 -0800

    Merged changes, added Switch to app router

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -62,7 +62,7 @@ export default function Navbar(props) {
                 </Link>
               </li>
               <li>
-                <Logout/>
+                <Logout />
               </li>
             </ul>
           </div>

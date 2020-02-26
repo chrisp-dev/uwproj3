152414c39221144456065a1e19f7fb38decc52a4
unknown <nathan.c.ginn@gmail.com>
2020-02-25 17:06:05 -0800

    added swipenav and profile button

---
+++ src/components/SwipeNav/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ProfileButton from '../ProfileButton';
+import Logout from '../Logout'
+import { Link } from 'react-router-dom';
+
+export default function ProfileNav(props) {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+    return (
+        <>
+            <nav
+                className={
+                    (props.transparent
+                        ? "top-0 absolute z-50 w-full"
+                        : "relative shadow-lg bg-white shadow-lg") +
+                    " flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+                }
+            >
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <Link
+                            className={
+                                (props.transparent ? "text-white" : "text-gray-800") +
+                                " text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
+                            }
+                            to="/"
+                        >
+                            Mosh
+            </Link>
+                        <button
+                            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <i
+                                className={
+                                    (props.transparent ? "text-white" : "text-gray-800") +
+                                    " fas fa-bars"
+                                }
+                            ></i>
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none" +
+                            (navbarOpen ? " block rounded shadow-lg" : " hidden")
+                        }
+                        id="example-navbar-warning">
+
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="flex items-center">
+                                <ProfileButton />
+                                <Logout />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}

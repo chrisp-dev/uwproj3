dda6c954550204199e0e524aff79738105821e60
jittel <nathan.c.ginn@gmail.com>
2020-02-27 14:24:13 -0800

    matches works kinda

--- src/components/Matches/message-bits/Head.js
+++ src/components/Matches/message-bits/Head.js
@@ -6,7 +6,7 @@ export default function Head({ matches }) {
     return (
         <div className='row flex horizontal' style={{ backgroundColor: 'white' }}>
             {/* TODO: make it so that it loads however many matches you have */}
-            {matches ? matches.map(match => <Image image={match.image} />) : null}
+            {matches ? matches.map(match => <Image image={match.imageUrl} />) : null}
         </div>
     )
 }

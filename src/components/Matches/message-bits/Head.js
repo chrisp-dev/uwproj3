be9aa38f7349d9fee530744e5ad18d74d8744878
jittel <nathan.c.ginn@gmail.com>
2020-02-27 15:38:19 -0800

    can see messages now

--- src/components/Matches/message-bits/Head.js
+++ src/components/Matches/message-bits/Head.js
@@ -5,7 +5,7 @@ import './style.css'
 export default function Head({ matches }) {
     return (
         <div className='row flex horizontal' style={{ backgroundColor: 'white' }}>
-            {/* TODO: make it so that it loads however many matches you have */}
+            {/* loads however many matches you have */}
             {matches ? matches.map(match => <Image image={match.imageUrl} name={match.firstName}/>) : null}
         </div>
     )

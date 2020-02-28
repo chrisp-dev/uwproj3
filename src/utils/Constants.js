b4c6916096e2e4cd80a28203c9df42d7f241a9a8
chrisp-dev <chris.paradeise@gmail.com>
2020-02-27 19:17:33 -0800

    WOrks?

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -4,7 +4,7 @@ const prod = {
 };
 
 const dev = {
-  API_URL: "http://localhost:8080"
+  API_URL: "https://mosh-api.herokuapp.com"
 };
 
 const config = process.env.NODE_ENV === "development" ? dev : prod;

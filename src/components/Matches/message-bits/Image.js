cd3cb4f441c3a1bbce725419a7fc998431e09692
Ari Tserendavaa <tserenda@hotmail.com>
2020-02-29 13:12:02 -0800

    changed class to className

--- src/components/Matches/message-bits/Image.js
+++ src/components/Matches/message-bits/Image.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import "./style.css";
 
 export default function Image({ image, name, matchDate }) {
-    return (
-        <div class="flex items-center">
-            <img class="w-10 h-10 rounded-full mr-4" src={image} alt={name} />
-            <div class="text-sm">
-                <p class="text-gray-900 leading-none">{name || "Jack Black"}</p>
-                <p class="text-gray-600">{matchDate || "Today 3:53pm"}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex items-center">
+      <img className="w-10 h-10 rounded-full mr-4" src={image} alt={name} />
+      <div className="text-sm">
+        <p className="text-gray-900 leading-none">{name || "Jack Black"}</p>
+        <p className="text-gray-600">{matchDate || "Today 3:53pm"}</p>
+      </div>
+    </div>
+  );
 }
